4434ecdcaf67cde4984b607a35dcf1a758f24b26
srkshirsagar2010-hub <srkshirsagar2010@gmail.com>
2025-11-15 17:49:52 +0530

    Add files via upload

---
+++ 100DAYSPYTHON_DAY1.py
@@ -0,0 +1,8 @@
+print("WELCOME TO BAND NAME GENERATOR")
+name = input("Which city did you grew up in?\n")
+
+
+username =input( "What is your pet name?\n")
+
+
+print(" The name of your Band is " + name + " " +  username)
